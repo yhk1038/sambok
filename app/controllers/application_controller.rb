95947c789e4861db4fd29e17cdb4c8e357923b91
yhk1038 <yhk1038@hanmail.net>
2016-12-29 01:31:56 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    # protect_from_forgery with: :exception
+    
+    $app_url    = "http://sambok161227-yhk1038.c9users.io"
+    $app_title  = "SAMBOK PRODUCTION"
+end
