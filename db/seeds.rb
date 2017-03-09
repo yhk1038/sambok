a2ab0f0c933bf976a1e03ab9abd78101eada9216
fred d kim <yhk1038@hanmail.com>
2017-03-10 05:53:27 +0900

    seed append

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Sambok.create!([
+  {name: "SAMBOK PRODUCTION", slogan: "M A K I N G__W H A T__ T H E__W O R L D__W A N T S", img_path: "/asset/header/img/sambok_text_logo.png"}
+])
+Section.create!([
+  {name: "about", subtitle: "", subtitle_size: nil, subtitle2: "", subtitle2_size: nil, content: "///청년영상제작크루 삼복프로덕션은 모바일에 특화된 영상콘텐츠를 만드는\r\n///20대 영상인들의 모임입니다.\r\n/// \r\n///복날이면 모두가 맛있고 영양가 있는 보양식을 찾듯이\r\n///모바일 영상이 필요한 곳에 보양식같은 영상을 만드는 것이 우리의 목표입니다.\r\n", img_path: "", bg_img_path: "#fa8c97"},
+  {name: "our work", subtitle: "", subtitle_size: nil, subtitle2: "movile video", subtitle2_size: nil, content: "///SNS를 중심으로 하는 모바일 영상 콘텐츠 시장에 발맞춰\r\n///삼복프로덕션은 스토리라인을 중심으로 하는 웹드라마, 웹무비를 제작합니다.\r\n///이를 통해, 우리는 세상이 원하는 이야기를 함께 만들어 나가고자 합니다.\r\n\r\n///또한, 바이럴 마케팅 영상이나 현장스케치 영상 등\r\n///다양한 사람들을 만나고, 소통할 수 있는 곳으로 삼복은 찾아갑니다.\r\n", img_path: "https://images.unsplash.com/photo-1423666639041-f56000c27a9a?h=9000&w=1200&q=90&fit=clip&fm=jpg", bg_img_path: "#fff"},
+  {name: "working process", subtitle: "Just suitable for your project", subtitle_size: nil, subtitle2: "기획부터 수정까지", subtitle2_size: nil, content: "///우리의 제작과정은 처음부터 끝까지 제작하려는 콘텐츠 위주로 구성됩니다.\r\n///기본 프로세스를 토대로 스토리라인에 따라 개별 프로세스를 세부 설정한 뒤에\r\n///기획을 시작하며, 컨펌 이후에도 2회에 걸쳐 추가 수정을 진행합니다.\r\n", img_path: "//res.cloudinary.com/hrscywv4p/image/upload/c_limit,h_9000,w_1200,f_auto/v1/1084360/22_ro6ur6.png", bg_img_path: "#35393e"},
+  {name: "contact", subtitle: "sambokproduction@naver.com", subtitle_size: nil, subtitle2: "", subtitle2_size: nil, content: "///서울시 중구 퇴계로36길 2, 131호(충무로영상센터)\r\n", img_path: "", bg_img_path: "#fa8c97"}
+])
